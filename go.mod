d6136ac1d55684d1eceae3dcbb8142acc9122184
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2021-10-31 19:31:48 +0900

    add

--- go.mod
+++ go.mod
@@ -1,8 +1,18 @@
 module github.com/EikoNakashima/sample-bun.git
 
-go 1.15
+go 1.17
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/uptrace/bun v1.0.12
+	github.com/uptrace/bun v1.0.13
+	github.com/uptrace/bun/dialect/mysqldialect v1.0.13
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
 )
--- main.go
+++ main.go
@@ -1,34 +1,123 @@
 package main
 
 import (
+	"context"
 	"database/sql"
-	"time"
+	"fmt"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/mysqldialect"
 )
 
 type User struct {
-	Id       int64 `bun:"id"`
-	Name     string
-	Age      int
-	Password string    `bun:"password"`
-	Created  time.Time `bun:"created"`
-	Updated  time.Time `bun:"updated"`
+	Id       int64  `bun:"id"`
+	Name     string `bun:"name"`
+	Age      int    `bun:"age"`
+	Password string `bun:"password"`
 }
 
 func main() {
 
-	sqldb, err := sql.Open("mysql", "root:root@tcp([127.0.0.1]:3306)/sample_db?charset=utf8mb4&parseTime=true")
+	engine, err := sql.Open("mysql", "root:root@tcp([127.0.0.1]:3306)/sample_db?charset=utf8mb4&parseTime=true")
 	if err != nil {
 		panic(err)
 	}
-	db := bun.NewDB(sqldb, mysqldialect.New())
+	db := bun.NewDB(engine, mysqldialect.New())
 
-	insert(db)
-
+	// 動かす時にコメントインする
+	// dropTable(db)
+	// createTable(db)
+	// insertOne(db)
+	// insertAll(db)
+	// getOne(db)
+	// getAll(db)
+	// delete(db)
+	update(db)
 }
 
-func insert(db) {
+func dropTable(db *bun.DB) {
+	_, err := db.NewDropTable().Model((*User)(nil)).Exec(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("dropTable")
+}
 
+func createTable(db *bun.DB) {
+	_, err := db.NewCreateTable().Model((*User)(nil)).Exec(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("createTable")
+}
+
+func insertOne(db *bun.DB) {
+	user := User{
+		Name:     "太郎",
+		Password: "パスワード",
+		Age:      20,
+	}
+
+	_, err := db.NewInsert().Model(&user).Exec(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("insertOne")
+}
+
+func insertAll(db *bun.DB) {
+	user2 := User{
+		Name:     "花子",
+		Password: "パスワード",
+		Age:      25,
+	}
+	user3 := User{
+		Name:     "りょう",
+		Password: "パスワード",
+		Age:      30,
+	}
+	users := []User{user2, user3}
+	_, err := db.NewInsert().Model(&users).Exec(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("insertAll")
+}
+
+func getOne(db *bun.DB) {
+	user := User{}
+	err := db.NewSelect().Model(&user).Where("id = 1").Scan(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("getOne", user)
+}
+
+func getAll(db *bun.DB) {
+	users := []User{}
+	err := db.NewSelect().Model(&users).OrderExpr("id ASC").Scan(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("getAll", users)
+}
+
+func delete(db *bun.DB) {
+	user := User{}
+	_, err := db.NewDelete().Model(&user).Where("id = 1").Exec(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("delete")
+}
+
+func update(db *bun.DB) {
+	user := User{}
+	_, err := db.NewUpdate().Model(&user).Set("age = 40").Where("id = ?", 2).Exec(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("update")
 }
